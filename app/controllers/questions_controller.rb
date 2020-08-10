4d1a0fd378a6d5d7cc699bbb72faede755cebdde
Marc Gaetcke <marc_onthemove@hotmail.com>
2020-08-10 15:21:26 +0200

    #2 80% done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    raise
+    @questions = params[:question]
+
+    if @questions.blank?
+      @answer = "I cant hear you!"
+    elsif @questions.downcase == "i am going to work"
+      @answer = "Great!"
+    elsif @questions.downcase.include?("?")
+      @answer = "Silly Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+
+  end
 end
